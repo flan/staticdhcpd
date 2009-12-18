a34f3a817b37a80c56dfdac141c87641c5391901
flonne <flonne@69f59fcc-fc11-11de-8802-f5e385a79211>
2009-12-18 16:30:25 +0000

    ...

--- src/web.py
+++ src/web.py
@@ -68,7 +68,6 @@ class _WebServer(BaseHTTPServer.BaseHTTPRequestHandler):
 				if key:
 					if hashlib.md5(key[0]).hexdigest() == conf.WEB_RELOAD_KEY:
 						try:
-							import conf
 							reload(conf)
 							src.logging.writeLog("Reloaded configuration")
 						except Exception, e:
