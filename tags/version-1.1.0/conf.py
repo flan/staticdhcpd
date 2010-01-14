524899ab907d0f582db1538b1903d6f8bfc0ae8a
red.hamsterx <red.hamsterx@69f59fcc-fc11-11de-8802-f5e385a79211>
2010-01-14 03:25:21 +0000

    Tagged 1.1.0.

---
+++ tags/version-1.1.0/conf.py
@@ -0,0 +1,145 @@
+#General settings
+#######################################
+#If True, all events will be printed to console.
+DEBUG = False
+
+#The name to use when referring to this system.
+SYSTEM_NAME = 'staticDHCPd'
+#The file to which logs should be dumped on receipt of TERM or HUP.
+LOG_FILE = '/var/log/' + SYSTEM_NAME + '.log'
+
+#The frequency at which the DHCP server's stats will be polled, in seconds.
+POLLING_INTERVAL = 30
+#The number of events to keep in the server's log-buffer.
+LOG_CAPACITY = 1000
+#The amount of statistical information to track.
+POLL_INTERVALS_TO_TRACK = 20
+
+#Server settings
+#######################################
+#The UID that will run this daemon.
+UID = 999
+#The GID that will run this daemon.
+GID = 999
+#The file to which PID information should be written.
+PID_FILE = '/var/run/' + SYSTEM_NAME + '.pid'
+
+#The IP of the interface on which DHCP responses should be sent.
+#This may be None to assume all interfaces, but this is not a good idea on a
+#multihomed server.
+DHCP_SERVER_IP = '192.168.1.100'
+#The port on which DHCP requests are to be received; 67 is the standard.
+DHCP_SERVER_PORT = 67
+#The port on which clients wait for DHCP responses; 68 is the standard.
+DHCP_CLIENT_PORT = 68
+
+#True to enable access to server statistics and logs.
+WEB_ENABLED = True
+#The IP of the interface on which the HTTP interface should be served.
+#Use '0.0.0.0' to serve on all interfaces.
+WEB_IP = '192.168.1.100'
+#The port on which the HTTP interface should be served.
+WEB_PORT = 30880
+
+#Server behaviour settings
+#######################################
+#If False, local DHCP requests will be ignored.
+ALLOW_LOCAL_DHCP = True
+#If False, relayed DHCP requests will be ignored.
+ALLOW_DHCP_RELAYS = False
+#A list of all IPs allowed to relay requests; if empty, all are allowed.
+#(End with trailing comma)
+ALLOWED_DHCP_RELAYS = ()
+
+#If True, REBIND and RENEW requests are NAKed when received, forcing clients to
+#either wait out their lease or return to the DISCOVER phase.
+NAK_RENEWALS = False
+
+#The number of seconds for which to ignore unknown MACs.
+UNAUTHORIZED_CLIENT_TIMEOUT = 60
+#The number of seconds for which to ignore potentially malicious MACs.
+MISBEHAVING_CLIENT_TIMEOUT = 150
+#If True, MACs requesting too many addresses will be flagged as malicious.
+ENABLE_SUSPEND = True
+#The number of times a well-behaved MAC can search for or request an IP
+#within the polling interval.
+SUSPEND_THRESHOLD = 10
+
+#MD5 of the password needed to reload config.
+WEB_RELOAD_KEY = '5f4dcc3b5aa765d61d8327deb882cf99'
+
+#Database settings
+#######################################
+#Allowed values: MySQL, SQLite
+DATABASE_ENGINE = 'MySQL'
+
+#Controls whether DHCP data gleaned from database lookups should be cached until
+#manually flushed; consumes more resources and adds a step before a MAC can be
+#automatically NAKed or have its details updated, but dramatically improves
+#performance under heavy loads.
+USE_CACHE = False
+
+#MYSQL_* values used only with 'MySQL' engine.
+#The name of your database.
+MYSQL_DATABASE = 'dhcp'
+#The name of a user with SELECT access.
+MYSQL_USERNAME = 'dhcp_user'
+#The password of the user.
+MYSQL_PASSWORD = 'dhcp_pass'
+#The host on which MySQL is running. None for 'localhost'.
+MYSQL_HOST = None
+#The port on which MySQL is running; ignored when HOST is None.
+MYSQL_PORT = 3306
+#The number of threads that may read the database at once.
+MYSQL_MAXIMUM_CONNECTIONS = 4
+
+#SQLITE_* values used only with 'SQLite' engine.
+#The file that contains your SQLite database.
+SQLITE_FILE = '/etc/staticDHCPd/dhcp.sqlite3'
+#The number of threads that may read the database at once.
+SQLITE_MAXIMUM_CONNECTIONS = 5
+
+#E-mail settings
+#######################################
+#True to allow staticDHCPd to inform you of any problems it cannot handle by
+#itself. (*Very* useful for development and fast troubleshooting)
+EMAIL_ENABLED = False
+#The server that receives your e-mail.
+EMAIL_SERVER = 'mail.yourdomain.com'
+#The user from which e-mail should claim to be sent.
+EMAIL_SOURCE = 'you@yourdomain.com'
+#The user to whom e-mail should be sent.
+EMAIL_DESTINATION = 'problems@yourdomain.com'
+#The user who should authenticate to the mail server.
+#If None, SMTP authentication is not used.
+EMAIL_USER = 'you'
+#The password of the user who should authenticate to the mail server.
+EMAIL_PASSWORD = 'password'
+#The number of seconds to wait between sending e-mails.
+EMAIL_TIMEOUT = 600
+
+#DHCP-processing functions
+#######################################
+#PERFORM ANY REQUIRED IMPORTS WITHIN init()
+def init():
+	from src.dhcp import ipToQuad, ipsToQuads, intToDouble, longToQuad, strToStrList
+#DEFINE ANY REQUIRED FUNCTIONS OR VARIABLES BELOW THIS LINE
+
+def loadDHCPPacket(packet, mac, client_ip, relay_ip, subnet, serial):
+	#This is a custom function, called before each packet is sent, that
+	#allows you to tweak the options attached to a DHCP response.
+	#
+	#If, for any reason, you want to abort sending the packet, return False.
+	#
+	#If you need to add, test for, or delete an option, consult staticDHCPd's
+	#rule-writing guide.
+	#
+	##### PARAMETERS #####
+	#mac is a human-readable MAC string, lower-case, separated by colons.
+	#client_ip is a quadruple of octets: (192, 168, 1, 1)
+	#relay_ip is either None or an address as a quadruple of octets,
+	#	depending on whether this is a response to a relay request.
+	#subnet and serial are values passed through from the database, as a
+	#	basestring and int, respectively.
+	return True
+	
---
+++ tags/version-1.1.0/pydhcplib/dhcp_basic_packet.py
@@ -0,0 +1,163 @@
+# -*- encoding: utf-8 -*-
+"""
+pydhcplib module: dhcp_basic_packet
+
+Purpose
+=======
+ Base class to encode/decode dhcp packets.
+ 
+Legal
+=====
+ This file is part of pydhcplib, but it has been altered for use with
+ staticDHCPd.
+ pydhcplib is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ 
+ (C) Mathieu Ignacio, 2008 <mignacio@april.org>
+"""
+import operator
+from struct import unpack
+from struct import pack
+
+from dhcp_constants import *
+
+class DhcpBasicPacket(object):
+	def __init__(self):
+		self.packet_data = [0]*240
+		self.options_data = {}
+		self.packet_data[236:240] = MagicCookie
+		self.source_address = False
+		self.requested_options = None
+		
+	def IsDhcpPacket(self):
+		return self.packet_data[236:240] == MagicCookie
+		
+	def CheckType(self, variable):
+		# Check if variable is a list of ints between 0 and 255
+		if type(variable) == list:
+			for each in variable:
+				if not type(each) == int or not 0 <= each <= 255:
+					return False
+			return True
+		return False
+		
+	def DeleteOption(self, name):
+		#zero-out the value if it is core to the DHCP packet, else just drop it
+		if DhcpFields.has_key(name):
+			dhcp_field = DhcpFields[name]
+			begin = dhcp_field[0]
+			end = dhcp_field[0] + dhcp_field[1]
+			self.packet_data[begin:end] = [0]*dhcp_field[1]
+			return True
+		elif self.options_data.has_key(name):
+			del self.options_data[name]
+			return True
+		return False
+		
+	def GetOption(self, name):
+		if DhcpFields.has_key(name):
+			option_info = DhcpFields[name]
+			return self.packet_data[option_info[0]:option_info[0] + option_info[1]]
+		elif self.options_data.has_key(name):
+			return self.options_data[name]
+		return []
+		
+	def SetOption(self, name, value):
+		#Basic value checking: does the value list have a valid length?
+		if DhcpFields.has_key(name):
+			dhcp_field = DhcpFields[name]
+			if not len(value) == dhcp_field[1]:
+				return False 
+			begin = dhcp_field[0]
+			end = dhcp_field[0] + dhcp_field[1]
+			self.packet_data[begin:end] = value
+			return True
+		elif DhcpOptions.has_key(name):
+			(fixed_length, minimum_length, multiple) = DhcpFieldsSpecs[DhcpOptionsTypes[DhcpOptions[name]]]
+			length = len(value)
+			if fixed_length == length or (minimum_length <= length and length % multiple == 0):
+				self.options_data[name] = value
+				return True
+			return False
+		raise ValueError("pydhcplib.dhcp_basic_packet.setoption error : unknown option: %(name)s" % {'name': name})
+		
+	def IsOption(self, name):
+		return self.options_data.has_key(name) or DhcpFields.has_key(name)
+		
+	def EncodePacket(self):
+		#MUST set options in ascending order to respect RFC2131 (see 'router')
+		options = {}
+		for each in self.options_data.keys():
+			option_id = DhcpOptions[each]
+			if self.requested_options is None or option_id in self.requested_options:
+				options[option_id] = option = []
+				option.append(option_id)
+				
+				option_value = self.options_data[each]
+				option.append(len(option_value))
+				option += option_value
+		ordered_options = []
+		for (option_id, value) in sorted(options.iteritems()):
+			ordered_options += value
+			
+		packet = self.packet_data[:240] + ordered_options
+		packet.append(255) # add end option
+		pack_fmt = str(len(packet)) + "c"
+		packet = map(chr, packet)
+		
+		return pack(pack_fmt, *packet)
+		
+	def DecodePacket(self, data):
+		if not data:
+			return False
+			
+		# we transform all data to int list
+		unpack_fmt = str(len(data)) + "c"
+		self.packet_data = [ord(i) for i in unpack(unpack_fmt, data)]
+		self.options_data = {}
+		
+		# Some servers or clients don't place magic cookie immediately
+		# after headers and begin options fields only after magic.
+		# These 4 lines search magic cookie and begin iterator after.
+		iterator = 236
+		end_iterator = len(self.packet_data)
+		while not self.packet_data[iterator:iterator + 4] == MagicCookie and iterator < end_iterator:
+			iterator += 1
+		iterator += 4
+		
+		# parse extended options
+		while iterator < end_iterator:
+			if self.packet_data[iterator] == 0: # pad option
+				opt_first = iterator + 1
+				iterator += 1
+			elif self.packet_data[iterator] == 255:
+				self.packet_data = self.packet_data[:240] # base packet length without magic cookie
+				return
+			elif DhcpOptionsTypes.has_key(self.packet_data[iterator]) and not self.packet_data[iterator] == 255:
+				opt_len = self.packet_data[iterator + 1]
+				opt_first = iterator + 1
+				opt_id = self.packet_data[iterator]
+				opt_val = self.packet_data[opt_first + 1:opt_len + opt_first + 1]
+				self.options_data[DhcpOptionsList[opt_id]] = opt_val
+				if opt_id == 55:
+					self.requested_options = tuple(set(
+					 [int(i) for i in opt_val] + [1, 3, 6, 15, 51, 53, 54, 58, 59]
+					))
+				iterator += self.packet_data[opt_first] + 2
+			else:
+				opt_first = iterator+1
+				iterator += self.packet_data[opt_first] + 2
+				
+		# cut packet_data to remove options
+		self.packet_data = self.packet_data[:240] # base packet length with magic cookie
+		
