f105dd222948e0970d921f42f58aa6181b951044
red.hamsterx <red.hamsterx@69f59fcc-fc11-11de-8802-f5e385a79211>
2010-02-11 07:44:37 +0000

    More commented and more optimized.

--- src/libpydhcpserver/type_ipv4.py
+++ src/libpydhcpserver/type_ipv4.py
@@ -27,6 +27,15 @@ Legal
 """
 # Check if _ip_numlist is valid.
 def checkNumList(value):
+	"""
+	Ensures that the given value is made up of four single bytes.
+	
+	@type value: sequence
+	@param value: The value to be tested.
+	
+	@rtype: bool
+	@return: True if validation succeeds.
+	"""
 	if not len(value) == 4:
 		return False
 	for part in value:
@@ -34,96 +43,139 @@ def checkNumList(value):
 			return False
 	return True
 	
-# Check if _ip_string is valid.
 def checkString(value):
-	tmp = value.strip().split('.')
-	if not len(tmp) == 4:
+	"""
+	Ensures that the given value is made up of four dot-delimited single bytes.
+	
+	@type value: basestring
+	@param value: The value to be tested.
+	
+	@rtype: bool
+	@return: True if validation succeeds.
+	"""
+	octets = value.strip().split('.')
+	if not len(octets) == 4:
 		return False
-	for each in tmp:
-		if not each.isdigit():
+	for o in octets:
+		if not o.isdigit():
 			return False
-		if not 0 <= int(each) <= 255:
+		if not 0 <= int(o) <= 255:
 			return False
 	return True
 	
 class ipv4(object):
-	def __init__(self, value="0.0.0.0") :
-		ip_type = type(value)
-		if ip_type == str:
-			if not checkString(value):
-				raise ValueError("ipv4 string argument '%(ip)s' is not a valid IP" % {'ip': value,})
-			self._ip_string = value
+	"""
+	Evaluates and encodes an IPv4 for use as part of a DHCP packet. 
+	"""
+	_ip_long = None #: A long-encoded IPv4.
+	_ip_numlist = None #: An encoded list of bytes.
+	_ip_string = None #: A human-readable string.
+	
+	def __init__(self, data="0.0.0.0"):
+		"""
+		Accepts data and ensures that both human-readable and packet-encodable
+		values are made available.
+		
+		@type data: str|list|tuple
+		@param data: The data to be processed.
+		
+		@raise TypeError: Unsupported data-type was supplied.
+		@raise ValueError: Invalid data supplied.
+		"""
+		if type(data) == str:
+			if not checkString(data):
+				raise ValueError("'%(ip)s' is not a valid IP" % {
+				 'ip': data,
+				})
+			self._ip_string = data
 			self._stringToNumlist()
-			self._stringToLong()
+			self._numlistToLong()
+		elif type(data) in (list, tuple):
+			if not checkNumList(data):
+				raise ValueError("'%(ip)s' is not a valid IP" % {
+				 'ip': str(data),
+				})
+			self._ip_numlist = data
 			self._numlistToString()
-		elif ip_type in (list, tuple):
-			if not checkNumList(value):
-				raise ValueError("ipv4 list argument '%(ip)s' is not a valid IP" % {'ip': str(value),})
-			self._ip_numlist = value
-			self._numlistToString()
-			self._stringToLong()
-		elif ip_type in (int, long):
-			self._ip_long = value
-			self._longToNumlist()
-			self._numlistToString()
-		elif ip_type == bool :
-			self._ip_long = 0
+			self._numlistToLong()
+		elif type(data) in (int, long):
+			if not 0 <= data <= 4294967295:
+				raise ValueError("'%(ip)i' is not a valid IP" % {
+				 'ip': data,
+				})
+			self._ip_long = data
 			self._longToNumlist()
 			self._numlistToString()
 		else:
-			raise TypeError('ipv4 init : expected str, list, or long; got %(type)s' % {'type': ip_type,})
+			raise TypeError('Expected str, list, or long; got %(type)s' % {
+			 'type': ip_type,
+			})
 			
-	# Convert Long type ip to numlist ip
 	def _longToNumlist(self):
+		"""
+		Converts a long value into a collection of bytes.
+		"""
 		self._ip_numlist = [self._ip_long >> 24 & 0xFF]
 		self._ip_numlist.append(self._ip_long >> 16 & 0xFF)
 		self._ip_numlist.append(self._ip_long >> 8 & 0xFF)
 		self._ip_numlist.append(self._ip_long & 0xFF)
-		if not checkNumList(self._ip_numlist):
-			raise ValueError("ipv4 long argument '%(ip)s' is not a valid IP" % {'ip': str(self._ip_numlist),})
-			
-	# Convert String type ip to Long type ip
-	def _stringToLong(self):
-		ip_numlist = map(int,self._ip_string.split('.'))
-		self._ip_long = ip_numlist[3] + ip_numlist[2]*256 + ip_numlist[1]*256*256 + ip_numlist[0]*256*256*256
-		if not checkNumList(self._ip_numlist):
-			raise ValueError("ipv4 string argument '%(ip)s' is not a valid IP" % {'ip': self._ip_numlist,})
-			
-	# Convert NumList type ip to String type ip
+		
+	def _numlistToLong(self):
+		"""
+		Converts a collection of bytes into a long value.
+		"""
+		self._ip_long = sum([x * 256 ** i for (i, x) in enumerate(reversed(self._ip_numlist))])
+		
 	def _numlistToString(self):
+		"""
+		Converts a collection of bytes into a human-readable string.
+		"""
 		self._ip_string = ".".join(map(str, self._ip_numlist))
-		if not checkNumList(self._ip_numlist):
-			raise ValueError("ipv4 list argument '%(ip)s' is not a valid IP" % {'ip': str(self._ip_numlist),})
-			
-	# Convert String type ip to NumList type ip
+		
 	def _stringToNumlist(self):
+		"""
+		Converts an IP string into a collection of bytes.
+		"""
 		self._ip_numlist = map(int, self._ip_string.split('.'))
-		if not checkNumList(self._ip_numlist):
-			raise ValueError("ipv4 string argument '%(ip)s' is not a valid IP" % {'ip': self._ip_string,})
-			
-	# return ip string
-	def str(self):
-		return self._ip_string
 		
-	# return ip list (useful for DhcpPacket class)
-	def list(self):
-		return self._ip_numlist
-		
-	# return Long ip type (useful for SQL ip address backend)
 	def int(self):
+		"""
+		Returns the integer data contained within this object.
+		
+		@rtype: int
+		@return: A long value.
+		"""
 		return self._ip_long
 		
-	def __hash__(self):
-		return self._ip_long.__hash__()
+	def list(self):
+		"""
+		Returns the packet-encodable data contained within this object.
 		
-	def __repr__(self):
+		@rtype: list
+		@return: A collection of bytes.
+		"""
+		return self._ip_numlist
+		
+	def str(self):
+		"""
+		Returns the human-readable data contained within this object.
+		
+		@rtype: str
+		@return: A human-readable value.
+		"""
 		return self._ip_string
 		
 	def __cmp__(self,other):
 		return cmp(self._ip_long, other._ip_long)
 		
+	def __hash__(self):
+		return self._ip_long.__hash__()
+		
 	def __nonzero__(self):
 		if self._ip_long:
 			return 1
 		return 0
+		
+	def __repr__(self):
+		return self._ip_string
 		
