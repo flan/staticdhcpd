d668eaee93c4759f8f922dcd65c01c3c8629b897
red.hamsterx <red.hamsterx@69f59fcc-fc11-11de-8802-f5e385a79211>
2013-08-20 19:31:25 +0000

    Added Debian build rules for libpydhcpserver and an automation script.

---
+++ makedebs.sh
@@ -0,0 +1,13 @@
+#Runs both libpydhcpserver and staticDHCPd's Debian scripts
+cd libpydhcpserver
+/usr/bin/debuild -uc -us
+cd ..
+
+#cd staticDHCPd
+#/usr/bin/debuild -uc -us
+#cd ..
+
+rm *.dsc
+rm *.changes
+rm *.build
+rm *.tar.gz
