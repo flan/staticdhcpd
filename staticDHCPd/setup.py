f08fd5ab6ddbd582f38929055c1d39c968ee7be6
red.hamsterx <red.hamsterx@69f59fcc-fc11-11de-8802-f5e385a79211>
2013-08-21 15:18:23 +0000

    Corrected capitalisation of control-scripts.
    Made setup.py Debian-aware to avoid useless prints.
    
    Marked executable scripts as executable in SVN.

--- staticDHCPd/setup.py
+++ staticDHCPd/setup.py
@@ -3,6 +3,7 @@
 Deployment script for staticDHCPd.
 """
 from distutils.core import setup
+import os
 import platform
 
 from staticdhcpdlib import VERSION
@@ -34,30 +35,31 @@ setup(
 )
 
 #Post-installation stuff
-instructions = [
-"",
-"",
-"Before you can run 'staticDHCPd', which should now be in PATH, you must copy",
-"/etc/staticDHCPd/conf.py.sample to /etc/staticDHCPd/conf.py and configure it",
-"",
-"Perform the following tasks to configure staticDHCPd to launch on system startup",
-]
-if platform.linux_distribution()[0] in ('Debian', 'Ubuntu', 'Mint',):
-    instructions.extend([
-"Debian-like (" + platform.linux_distribution()[0] + ":" + platform.linux_distribution()[1] + ")",
-"\tCopy samples/staticDHCPd to /etc/init.d/staticDHCPd",
-"\tRun '/bin/chmod a+x /etc/init.d/staticDHCPd'",
-"\tRun '/usr/sbin/update-rc.d staticDHCPd defaults'",
-    ])
-elif platform.mac_ver()[0]:
-    instructions.extend([
-"OS X",
-"\tCopy samples/ca.uguu.puukusoft.staticdhcpd.plist to /Library/LaunchDaemons/"
-    ])
-else:
-    instructions.extend([
-"Instructions relevant to your platform are unavailable; please contribute documentation",
-    ])
+if os.getenv('DEBUILD_MODE') != 'yes': #Don't print confusing stuff when building Debian packages
+    instructions = [
+    "",
+    "",
+    "Before you can run 'staticDHCPd', which should now be in PATH, you must copy",
+    "/etc/staticDHCPd/conf.py.sample to /etc/staticDHCPd/conf.py and configure it",
+    "",
+    "Perform the following tasks to configure staticDHCPd to launch on system startup",
+    ]
+    if platform.linux_distribution()[0] in ('Debian', 'Ubuntu', 'Mint',):
+        instructions.extend([
+    "Debian-like (" + platform.linux_distribution()[0] + ":" + platform.linux_distribution()[1] + ")",
+    "\tCopy control-scripts/staticDHCPd to /etc/init.d/staticDHCPd",
+    "\tRun '/usr/sbin/update-rc.d staticDHCPd defaults'",
+        ])
+    elif platform.mac_ver()[0]:
+        instructions.extend([
+    "OS X",
+    "\tCopy control-scripts/ca.uguu.puukusoft.staticDHCPd.plist to /Library/LaunchDaemons/"
+        ])
+    else:
+        instructions.extend([
+    "Instructions relevant to your platform are unavailable; please contribute documentation",
+        ])
 
-for i in instructions:
-    print(i)
+    for i in instructions:
+        print(i)
+        
