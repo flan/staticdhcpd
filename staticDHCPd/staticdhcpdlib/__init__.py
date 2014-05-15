58ffe9809f8f107610ab53cb7d92c1b2ec47ccc5
red.hamsterx <red.hamsterx@69f59fcc-fc11-11de-8802-f5e385a79211>
2014-05-15 20:41:27 +0000

    Version bumped to -11.

--- staticDHCPd/staticdhcpdlib/__init__.py
+++ staticDHCPd/staticdhcpdlib/__init__.py
@@ -22,8 +22,8 @@ Legal
  You should have received a copy of the GNU General Public License
  along with this program. If not, see <http://www.gnu.org/licenses/>.
  
- (C) Neil Tallim, 2013 <flan@uguu.ca>
+ (C) Neil Tallim, 2014 <flan@uguu.ca>
 """
-VERSION = '2.0.0-beta-10'
+VERSION = '2.0.0-beta-11'
 URL = 'http://uguu.ca/'
-COPYRIGHT = '2013, Neil Tallim <flan@uguu.ca>'
+COPYRIGHT = '2014, Neil Tallim <flan@uguu.ca>'
