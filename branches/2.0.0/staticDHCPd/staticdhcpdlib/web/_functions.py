0283658237e1970b59379b749745b0c8bcb1f75a
red.hamsterx@gmail.com <red.hamsterx@gmail.com@69f59fcc-fc11-11de-8802-f5e385a79211>
2014-06-22 19:32:59 +0000

    More documentations.

--- branches/2.0.0/staticDHCPd/staticdhcpdlib/web/_functions.py
+++ branches/2.0.0/staticDHCPd/staticdhcpdlib/web/_functions.py
@@ -1,35 +1,35 @@
 # -*- encoding: utf-8 -*-
 """
-staticDHCPd module: web._functions
-
-Purpose
-=======
- Provides functions required to transform content for web-presentation.
+staticdhcpdlib.web._functions
+=============================
+Provides functions required to transform content for web-presentation.
  
 Legal
-=====
- This file is part of staticDHCPd.
- staticDHCPd is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
+-----
+This file is part of staticDHCPd.
+staticDHCPd is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <http://www.gnu.org/licenses/>.
- 
- (C) Neil Tallim, 2013 <flan@uguu.ca>
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+(C) Neil Tallim, 2014 <flan@uguu.ca>
 """
 import cgi
 
 def sanitise(string):
     """
-    `Ensures that the string, if not None or empty, is usable anywhere in an
-    HTML5 body.
+    Ensures that the string is usable anywhere in an HTML5 body.
+    
+    :param basestring string: The string to sanitise.
+    :return basestring: The sanitised string, or None if nothing was provided.
     """
     return string and cgi.escape(string).replace('"', '&quot;')
     
--- branches/2.0.0/staticDHCPd/staticdhcpdlib/web/headers.py
+++ branches/2.0.0/staticDHCPd/staticdhcpdlib/web/headers.py
@@ -1,28 +1,26 @@
 # -*- encoding: utf-8 -*-
 """
-staticDHCPd module: web.headers
+staticdhcpdlib.web.headers
+==========================
+Provides implementations of the default <head/> elements.
 
-Purpose
-=======
- Provides implementations of the default <head/> elements.
- 
 Legal
-=====
- This file is part of staticDHCPd.
- staticDHCPd is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
+-----
+This file is part of staticDHCPd.
+staticDHCPd is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <http://www.gnu.org/licenses/>.
- 
- (C) Neil Tallim, 2013 <flan@uguu.ca>
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+(C) Neil Tallim, 2014 <flan@uguu.ca>
 """
 import logging
 
@@ -32,18 +30,43 @@ import _functions
 _logger = logging.getLogger('web.headers')
 
 def contentType(*args, **kwargs):
+    """
+    Provides the default content-type HTML header.
+    
+    :return str: The content-type header.
+    """
     return '<meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8"/>'
     
-_TITLE = '<title>' + _functions.sanitise(config.SYSTEM_NAME) + '</title>'
+_TITLE = '<title>' + _functions.sanitise(config.SYSTEM_NAME) + '</title>' #: The title of the web interface
 def title(*args, **kwargs):
+    """
+    Provides the default title HTML header.
+    
+    :return str: The title header.
+    """
     return _TITLE
     
 def css(*args, **kwargs):
+    """
+    Provides the default CSS HTML header.
+    
+    :return str: The CSS header.
+    """
     return '<link rel="stylesheet" type="text/css" href="/css"/>'
     
 def favicon(*args, **kwargs):
+    """
+    Provides the default favicon HTML header.
+    
+    :return str: The favicon header.
+    """
     return '<link rel="icon" type="image/x-icon" href="/favicon.ico"/>'
     
 def javascript(*args, **kwargs):
+    """
+    Provides the default JavaScript HTML header.
+    
+    :return str: The JavaScript header.
+    """
     return '<script type="text/javascript" src="/javascript"></script>'
     
