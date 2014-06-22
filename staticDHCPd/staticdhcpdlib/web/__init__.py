03f8bfb868e66f2241c777ce11c977c51bf643fe
red.hamsterx@gmail.com <red.hamsterx@gmail.com@69f59fcc-fc11-11de-8802-f5e385a79211>
2014-06-22 22:10:49 +0000

    Documented the web subsystem.

--- staticDHCPd/staticdhcpdlib/web/__init__.py
+++ staticDHCPd/staticdhcpdlib/web/__init__.py
@@ -1,28 +1,26 @@
 # -*- encoding: utf-8 -*-
 """
-staticDHCPd package: web
-
-Purpose
-=======
- Defines web-registration methods and structures.
+staticdhcpdlib.web
+==================
+Defines web-registration methods and structures.
  
 Legal
-=====
- This file is part of staticDHCPd.
- staticDHCPd is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
+-----
+This file is part of staticDHCPd.
+staticDHCPd is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <http://www.gnu.org/licenses/>.
- 
- (C) Neil Tallim, 2013 <flan@uguu.ca>
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+(C) Neil Tallim, 2014 <flan@uguu.ca>
 """
 import collections
 import logging
@@ -38,28 +36,85 @@ _web_dashboard = []
 _web_methods = {}
 
 _WebDashboardElement = collections.namedtuple("WebDashboardElement", ('ordering', 'module', 'name', 'callback'))
+"""
+A component of the dashboard.
+
+.. py:attribute:: ordering
+
+    An integer used to sort this element against others
+
+.. py:attribute:: module
+
+    The name of the module to which this element belongs
+
+.. py:attribute:: name
+
+    The name under which to display the element
+
+.. py:attribute:: callback
+
+    The method to invoke when rendering this element
+"""
 _WebMethod = collections.namedtuple("WebMethod", (
  'module', 'name', 'hidden', 'secure', 'confirm', 'display_mode', 'cacheable', 'callback'
 ))
+"""
+An invokable method.
+
+.. py:attribute:: module
+
+    The name of the module to which this method belongs
+
+.. py:attribute:: name
+
+    The name under which to display the method
+
+.. py:attribute:: hidden
+
+    Whether the method should be advertised on the dashboard
+
+.. py:attribute:: secure
+
+    Whether the method requires authorization
+
+.. py:attribute:: confirm
+
+    Whether the method, when invoked from the dashboard, requires confirmation
+
+.. py:attribute:: display_mode
+
+    Whether the method's callback presents information to display as part of
+    the dashboard, on its own, or as raw bytes
+
+.. py:attribute:: cacheable
+
+    Whether the method's response can be cached
+
+.. py:attribute:: callback
+
+    The method to invoke when rendering this element
+"""
 
 #Method-rendering constants
-WEB_METHOD_DASHBOARD = 1 #The content is rendered before the dashboard
-WEB_METHOD_TEMPLATE = 2 #The content is rendered in the same container that would normally show the dashboard, but no dashboard elements are present
-WEB_METHOD_RAW = 3 #The content is presented exactly as returned, identified by the given mimetype
+WEB_METHOD_DASHBOARD = 1 #: The content is rendered before the dashboard
+WEB_METHOD_TEMPLATE = 2 #: The content is rendered in the same container that would normally show the dashboard, but no dashboard elements are present
+WEB_METHOD_RAW = 3 #: The content is presented exactly as returned, identified by the given MIME-type
 
 def registerHeaderCallback(callback):
     """
     Installs an element in the headers; at most one instance of any given
-    `callback` will be accepted.
+    ``callback`` will be accepted.
     
-    The `callback` must accept the parameters 'path', 'queryargs', 'mimetype',
-    'data', and 'headers', with the possibility that 'mimetype' and 'data' may
-    be None; 'queryargs' is a dictionary of parsed query-string items, with
-    values expressed as lists of strings; 'headers' is a
-    `Python BasicHTTPServer` headers object.
-    
-    It must return data as a string, formatted as XHTML, to be embedded inside
-    of <head/>, or None to suppress inclusion.
+    :param callable callback: Must accept the parameters `path`, `queryargs`,
+                              `mimetype`, `data`, and `headers`, with the
+                              possibility that `mimetype` and `data` may be
+                              None; `queryargs` is a dictionary of parsed
+                              query-string items, with values expressed as lists
+                              of strings; `headers` is a dictionary-like object.
+                              
+                              It must return data as a string, formatted as
+                              XHTML, to be embedded inside of <head/>, or None
+                              to suppress inclusion.
     """
     with _web_lock:
         if callback in _web_headers:
@@ -70,19 +125,26 @@ def registerHeaderCallback(callback):
             
 def unregisterHeaderCallback(callback):
     """
-    Removes the specified element, identified by `callback, from the headers.
+    Removes a header element.
+    
+    :param callable callback: The element to be removed.
+    :return bool: True if an element was removed.
     """
     with _web_lock:
         try:
             _web_headers.remove(callback)
         except ValueError:
             _logger.error("header %(callback)r is not registered" % {'callback': callback,})
+            return False
         else:
             _logger.error("header %(callback)r unregistered" % {'callback': callback,})
+            return True
             
 def retrieveHeaderCallbacks():
     """
-    Returns every registered callback, in registration-order.
+    Enumerates header callbacks.
+    
+    :return tuple: All header callbacks, in registration order.
     """
     with _web_lock:
         return tuple(_web_headers)
@@ -90,24 +152,25 @@ def retrieveHeaderCallbacks():
 def registerDashboardCallback(module, name, callback, ordering=None):
     """
     Installs an element in the dashboard; at most one instance of any given
-    `callback` will be accepted.
+    ``callback`` will be accepted.
     
-    `module` and `name` describe how it will be presented, both as
-    human-readable strings.
-    
-    The `callback` must accept the parameters 'path', 'queryargs', 'mimetype',
-    'data', and 'headers', with the possibility that 'mimetype' and 'data' may
-    be None; 'queryargs' is a dictionary of parsed query-string items, with
-    values expressed as lists of strings; 'headers' is a
-    `Python BasicHTTPServer` headers object.
-    
-    It must return data as a string, formatted as XHTML, to be embedded inside
-    of a <div/>, or None to suppress inclusion.
-    
-    To control where the element will appear, supply `ordering`. This is a
-    number that controls where this element will appear in relation to
-    others. If omitted, the value will be that of the highest number plus one,
-    placing it at the end; negatives are valid.
+    :param basestring module: The name of the module to which this element
+                              belongs.
+    :param basestring name: The name under which to display the element.
+    :param callable callback: Must accept the parameters `path`, `queryargs`,
+                              `mimetype`, `data`, and `headers`, with the
+                              possibility that `mimetype` and `data` may be
+                              None; `queryargs` is a dictionary of parsed
+                              query-string items, with values expressed as lists
+                              of strings; `headers` is a dictionary-like object.
+                              
+                              It must return data as a string, formatted as
+                              XHTML, to be embedded inside of a <div/>, or None
+                              to suppress inclusion.
+    :param int ordering: A number that controls where this element will appear
+                         in relation to others. If not specified, the value will
+                         be that of the highest number plus one, placing it at
+                         the end; negatives are valid.
     """
     with _web_lock:
         for (i, element) in enumerate(_web_dashboard):
@@ -127,57 +190,59 @@ def registerDashboardCallback(module, name, callback, ordering=None):
             
 def unregisterDashboardCallback(callback):
     """
-    Removes the specified element, identified by `callback, from the dashboard.
+    Removes a dashboard element.
+    
+    :param callable callback: The element to be removed.
+    :return bool: True if an element was removed.
     """
     with _web_lock:
         for (i, element) in enumerate(_web_dashboard):
             if element.callback is callback:
                 del _web_dashboard[i]
                 _logger.debug("Unregistered dashboard element %(element)r" % {'element': element,})
-                break
-            else:
-                _logger.error("Dashboard callback %(callback)r is not registered" % {'callback': callback,})
-                
+                return True
+        else:
+            _logger.error("Dashboard callback %(callback)r is not registered" % {'callback': callback,})
+            return False
+            
 def retrieveDashboardCallbacks():
     """
-    Returns every registered callback, in display-order.
+    Enumerates dashboard callbacks.
+    
+    :return tuple: All dashboard callbacks, in display order.
     """
     with _web_lock:
         return tuple(_web_dashboard)
         
 def registerMethodCallback(path, callback, cacheable=False, hidden=True, secure=False, module=None, name=None, confirm=False, display_mode=WEB_METHOD_RAW):
     """
-    Installs a webservice method; at most one instance of `path` will be
+    Installs a webservice method; at most one instance of ``path`` will be
     accepted.
     
-    `path` is the location at which the service may be called, like
-    "/ca/uguu/puukusoft/staticDHCPd/extension/stats/histograph.csv".
-    
-    The `callback` must accept the parameters 'path', 'queryargs', 'mimetype',
-    'data', and 'headers', with the possibility that 'mimetype' and 'data' may
-    be None; 'queryargs' is a dictionary of parsed query-string items, with
-    values expressed as lists of strings; 'headers' is a
-    `Python BasicHTTPServer` headers object.
-    
-    It must return a tuple of (mimetype, data, headers), with data being a
-    string or bytes-like object.
-    
-    `cacheable` controls whether the client is allowed to cache the method's
-    content.
-    
-    `hidden` methods are not rendered in the side-bar.
-    
-    `secure` controls whether DIGEST authentication will be required before this
-    method can be called.
-    
-    `module` and `name` describe how it will be presented, both as
-    human-readable strings. Only if `hidden` is False, though.
-    
-    `confirm` adds JavaScript validation to ask the user if they're sure they
-    know what they're doing before the method will be invoked, if not `hidden`.
-    
-    `display_mode` is one of the WEB_METHOD_* constants, described at the start
-    of this module.
+    :param basestring path: The location at which the service may be called,
+        like "/ca/uguu/puukusoft/staticDHCPd/extension/stats/histograph.csv".
+    :param callable callback: Must accept the parameters `path`, `queryargs`,
+                              `mimetype`, `data`, and `headers`, with the
+                              possibility that `mimetype` and `data` may be
+                              None; `queryargs` is a dictionary of parsed
+                              query-string items, with values expressed as lists
+                              of strings; `headers` is a dictionary-like object.
+                              
+                              It must return a tuple of (mimetype, data,
+                              headers), with data being a string or bytes-like
+                              object.
+    :param bool cacheable: Whether the client is allowed to cache the method's
+                           content.
+    :param bool hidden: Whether to render a link in the side-bar.
+    :param bool secure: Whether authentication will be required before this
+                        method can be called.
+    :param basestring module: The name of the module to which this element
+                              belongs.
+    :param basestring name: The name under which to display the element.
+    :param bool confirm: Adds JavaScript validation to ask the user if they're
+                         sure they know what they're doing before the method
+                         will be invoked, if not `hidden`.
+    :param display_mode: One of the WEB_METHOD_* constants.
     """
     with _web_lock:
         if path in _web_methods:
@@ -191,25 +256,36 @@ def registerMethodCallback(path, callback, cacheable=False, hidden=True, secure=
             
 def unregisterMethodCallback(path):
     """
-    Removes the method registered at `path`.
+    Removes a method element.
+    
+    :param basestring path: The element to be removed.
+    :return bool: True if an element was removed.
     """
     with _web_lock:
         try:
             del _web_methods[path]
-            _logger.debug("Unregistered method %(path)s" % {'path': path,})
         except KeyError:
             _logger.error("'%(path)s' is not registered" % {'path': path,})
+            return False
+        else:
+            _logger.debug("Unregistered method %(path)s" % {'path': path,})
+            return True
             
 def retrieveMethodCallback(path):
     """
-    Returns the `_WebMethod` registered at `path`, if one exists.
+    Retrieves a method callback.
+    
+    :return callable: The requested method, or None if unbound.
     """
     with _web_lock:
         return _web_methods.get(path)
         
 def retrieveVisibleMethodCallbacks():
     """
-    Returns all visible (`_WebMethod`, `path`)s, in lexically sorted order.
+    Enumerates method callbacks.
+    
+    :return tuple: All method callbacks, as (`callback`, `path`) tuples, in
+                  lexically sorted order.
     """
     with _web_lock:
         return tuple(sorted((element, path) for (path, element) in _web_methods.items() if not element.hidden))
