e7b7042d904a98307234d0e6ba28cff42ff2ab03
red.hamsterx <red.hamsterx@69f59fcc-fc11-11de-8802-f5e385a79211>
2014-07-25 22:11:16 +0000

    Updated feedservice to use new configuration interface.

--- staticDHCPd/extensions/official/feedservice.py
+++ staticDHCPd/extensions/official/feedservice.py
@@ -5,55 +5,64 @@ events of importance, without all the noise that often accompanies e-mail
 updates, especially when something fails on every single request, like someone
 pulling a cable they shouldn't have touched.
 
-To use this module, customise the constants below, then add the following to
-conf.py's init() function:
+To use this module, configure whatever is required in conf.py, alongside
+staticDHCPd's built-in parameters, like this:
+    with extensions.feedservice as x:
+        #FEED_ID is something you should set uniquely on each server you run
+        x.FEED_ID = 'bcd2dbbc-105b-4533-bb6d-01c2333cc55e'
+        #This is usually intelligently inferred, but you might need to set it
+        #Trailing slash is optional
+        x.SERVER_BASE = 'http://someserver:someport'
+        
+For a list of all parameters you may define, see below.
+
+Then add the following to conf.py's init() function:
     import feedservice
     
 Like staticDHCPd, this module under the GNU General Public License v3
-(C) Neil Tallim, 2013 <flan@uguu.ca>
+(C) Neil Tallim, 2014 <flan@uguu.ca>
 """
-import staticdhcpdlib.config as config
-################################################################################
-#Do not touch anything above this line
-
-#The address at which your server can be reached
-#Defaults to the given web-address, if not '0.0.0.0'; 'localhost' otherwise
-#Rewrite it as needed
-_server_ip = (config.WEB_IP != '0.0.0.0' and config.WEB_IP or 'localhost')
-SERVER_BASE = 'http://' + _server_ip + ':' + str(config.WEB_PORT)
-
-#The minimum severity of events to include
-#Choices: 'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'
-LOGGING_LEVEL = 'ERROR'
-
-#The number of events to serve in a feed
-MAX_EVENTS_FEED = 10
-#The number of events to retain for linking purposes
-MAX_EVENTS_TOTAL = 100
-#The maximum age of an event that can be included in a feed
-MAX_AGE = 60 * 60 * 24
-
-#Whether feed items should be removed when the system is reinitialised
-CLEAR_ON_REINIT = True
-
-#The name to give the feed
-FEED_TITLE = config.SYSTEM_NAME
-
-#The path at which individual records will be served
-PATH_RECORD = '/ca/uguu/puukusoft/staticDHCPd/extension/feedservice/record'
-
-#The path at which to serve Atom (None to disable)
-PATH_ATOM = '/ca/uguu/puukusoft/staticDHCPd/extension/feedservice/atom'
-
-#Whether feeds should be advertised in the web interface's headers
-ADVERTISE = True
-
-#The ID that uniquely identifies this feed
-#If you are running multiple instances of staticDHCPd in your environment,
-#you MUST generate a new feed-ID for EACH server. To do this, execute the
-#following code in Python:
-#import uuid; str(uuid.uuid4())
-FEED_ID = 'bcd2dbbc-105b-4533-bb6d-01c2333cc55e'
+from staticdhcpdlib import config
+_config = config.extensions.feedservice
+_CONFIG = _config.extension_config_merge(defaults={
+    #The address at which your server can be reached
+    #Defaults to the given web-address, if not '0.0.0.0'; 'localhost' otherwise
+    'SERVER_BASE': 'http://' + (config.WEB_IP != '0.0.0.0' and config.WEB_IP or 'localhost') + ':' + str(config.WEB_PORT),
+    
+    #The minimum severity of events to include
+    #Choices: 'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'
+    'LOGGING_LEVEL': 'ERROR',
+    
+    #The number of events to serve in a feed
+    'MAX_EVENTS_FEED': 10,
+    #The number of events to retain for linking purposes
+    'MAX_EVENTS_TOTAL': 100,
+    #The maximum age of an event that can be included in a feed
+    'MAX_AGE': 60 * 60 * 24,
+    
+    #Whether feed items should be removed when the system is reinitialised
+    'CLEAR_ON_REINIT': True,
+    
+    #The name to give the feed
+    'FEED_TITLE': config.SYSTEM_NAME,
+    
+    #The path at which individual records will be served
+    'PATH_RECORD': '/ca/uguu/puukusoft/staticDHCPd/extension/feedservice/record',
+    
+    #The path at which to serve Atom (None to disable)
+    'PATH_ATOM': '/ca/uguu/puukusoft/staticDHCPd/extension/feedservice/atom',
+    
+    #Whether feeds should be advertised in the web interface's headers
+    'ADVERTISE': True,
+    
+    #The ID that uniquely identifies this feed
+    #If you are running multiple instances of staticDHCPd in your environment,
+    #you MUST generate a new feed-ID for EACH server. To do this, execute the
+    #following code in Python: import uuid; str(uuid.uuid4())
+    'FEED_ID': 'bcd2dbbc-105b-4533-bb6d-01c2333cc55e',
+}, required=[
+])
+del _config
 
 #Do not touch anything below this line
 ################################################################################
@@ -67,8 +76,10 @@ import xml.etree.ElementTree as ET
 
 _logger = logging.getLogger('extension.feedservice')
 
-_SERVER_ROOT = SERVER_BASE + '/'
-_RECORD_URI = SERVER_BASE + PATH_RECORD + "?uid=%(uid)s"
+if _CONFIG['SERVER_BASE'].endswith('/'):
+    _CONFIG['SERVER_BASE'] = _CONFIG['SERVER_BASE'][:-1]
+_SERVER_ROOT = _CONFIG['SERVER_BASE'] + '/'
+_RECORD_URI = _CONFIG['SERVER_BASE'] + _CONFIG['PATH_RECORD'] + "?uid=%(uid)s"
 
 _last_update = int(time.time())
 
@@ -173,7 +184,7 @@ def _feed_presenter(feed_type):
     return decorator
     
 _ATOM_ID_FORMAT = 'urn:uuid:%(id)s'
-_FEED_ID = _ATOM_ID_FORMAT % {'id': FEED_ID}
+_FEED_ID = _ATOM_ID_FORMAT % {'id': _CONFIG['FEED_ID']}
 @_feed_presenter('Atom')
 def _present_atom(logger):
     """
@@ -183,7 +194,7 @@ def _present_atom(logger):
     feed.attrib['xmlns'] = 'http://www.w3.org/2005/Atom'
     
     title = ET.SubElement(feed, 'title')
-    title.text = FEED_TITLE
+    title.text = _CONFIG['FEED_TITLE']
     link = ET.SubElement(feed, 'link')
     link.attrib['href'] = _SERVER_ROOT
     updated = ET.SubElement(feed, 'updated')
@@ -192,8 +203,8 @@ def _present_atom(logger):
     id.text = _FEED_ID
     
     global _ATOM_ID_FORMAT
-    max_age = time.time() - MAX_AGE
-    for element in logger.enumerateRecords(MAX_EVENTS_FEED):
+    max_age = time.time() - _CONFIG['MAX_AGE']
+    for element in logger.enumerateRecords(_CONFIG['MAX_EVENTS_FEED']):
         if element.timestamp < max_age: #Anything after this is also too old
             break
             
@@ -213,36 +224,39 @@ def _present_atom(logger):
     
 #Setup happens here
 ################################################################################
-_LOGGER = _FeedHandler(MAX_EVENTS_TOTAL)
+_LOGGER = _FeedHandler(_CONFIG['MAX_EVENTS_TOTAL'])
 _logger.info("Prepared feed-handler")
 _LOGGER.setFormatter(logging.Formatter("""%(asctime)s : %(levelname)s : %(name)s:%(lineno)d[%(threadName)s]
 <br/><br/>
 %(message)s"""))
-_LOGGER.setLevel(getattr(logging, LOGGING_LEVEL))
+_LOGGER.setLevel(getattr(logging, _CONFIG['LOGGING_LEVEL']))
 _logger.info("Feed-handler logging-level set to %(level)s" % {
- 'level': LOGGING_LEVEL,
+ 'level': _CONFIG['LOGGING_LEVEL'],
 })
 _logger.root.addHandler(_LOGGER)
 
-if CLEAR_ON_REINIT:
+if _CONFIG['CLEAR_ON_REINIT']:
     _logger.info("Registering callback handler to clear feeds on reinitialisation...")
     config.callbacks.systemAddReinitHandler(_LOGGER.flush)
     
 _logger.info("Registering record-access-point at '%(path)s'..." % {
- 'path': PATH_RECORD,
+ 'path': _CONFIG['PATH_RECORD'],
 })
-config.callbacks.webAddMethod(PATH_RECORD, _LOGGER.presentRecord, display_mode=config.callbacks.WEB_METHOD_TEMPLATE)
+config.callbacks.webAddMethod(
+ _CONFIG['PATH_RECORD'], _LOGGER.presentRecord,
+ display_mode=config.callbacks.WEB_METHOD_TEMPLATE
+)
 
-if PATH_ATOM:
+if _CONFIG['PATH_ATOM']:
     _logger.info("Registering Atom feed at '%(path)s'..." % {
-     'path': PATH_ATOM,
+     'path': _CONFIG['PATH_ATOM'],
     })
     config.callbacks.webAddMethod(
-     PATH_ATOM, lambda *args, **kwargs: _present_atom(_LOGGER),
+     _CONFIG['PATH_ATOM'], lambda *args, **kwargs: _present_atom(_LOGGER),
      display_mode=config.callbacks.WEB_METHOD_RAW
     )
-    if ADVERTISE:
+    if _CONFIG['ADVERTISE']:
         _logger.info("Adding reference to Atom feed to headers...")
-        atom_header = '<link href="' + PATH_ATOM + '" type="application/atom+xml" rel="alternate" title="' + config.SYSTEM_NAME + ' Atom"/>'
+        atom_header = '<link href="' + _CONFIG['PATH_ATOM'] + '" type="application/atom+xml" rel="alternate" title="' + config.SYSTEM_NAME + ' Atom"/>'
         config.callbacks.webAddHeader(lambda *args, **kwargs: atom_header)
         
