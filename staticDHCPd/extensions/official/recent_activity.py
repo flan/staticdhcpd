602508fe657ab7651cf494fa4906d68872626db9
red.hamsterx <red.hamsterx@69f59fcc-fc11-11de-8802-f5e385a79211>
2014-07-25 21:47:23 +0000

    Updated the recent_activity extension to use the new configuration interface.

--- staticDHCPd/extensions/official/recent_activity.py
+++ staticDHCPd/extensions/official/recent_activity.py
@@ -3,38 +3,51 @@
 Provides a means of easily tracking recent DHCP activity, so you can see if a
 device is self-configuring properly without needing to read through the logs.
 
-To use this module, customise the constants below, then add the following to
-conf.py's init() function:
+To use this module, configure whatever is required in conf.py, alongside
+staticDHCPd's built-in parameters, like this:
+    with extensions.recent_activity as x:
+        x.LIFETIME_STATS_ENABLED = True
+        
+For a list of all parameters you may define, see below.
+
+Then add the following to conf.py's init() function:
     import recent_activity
     
 Like staticDHCPd, this module under the GNU General Public License v3
-(C) Neil Tallim, 2013 <flan@uguu.ca>
+(C) Neil Tallim, 2014 <flan@uguu.ca>
 
 Inspiration derived from a discussion with John Stowers
 """
-#The number of events to track; if None, no limit will be applied
-MAX_EVENTS = 10
-#The maximum age of an event to track; if None, no limit will be applied
-MAX_AGE = 60 * 5
-
-#Whether feed items should be removed when the system is reinitialised
-CLEAR_ON_REINIT = True
-
-#Whether the list should be part of the dashboard; if False, it will appear
-#in the methods list
-DISPLAY_IN_DASHBOARD = True
-#The positioning bias of this element in the dashboard, as an integer
-#If None, it will appear at the end
-DASHBOARD_ORDERING = None
-#If registering as a method, this is where its callback will be registered
-METHOD_PATH = '/ca/uguu/puukusoft/staticDHCPd/extension/recent-activity/render'
-
-#The name of the module to which this element belongs
-MODULE = 'recent activity'
-#The name of this component
-NAME = 'dhcp'
-#If either of these are None and the module is not to be rendered in the
-#dashboard, the method-link will be hidden
+from staticdhcpdlib import config
+_config = config.extensions.statistics
+_CONFIG = _config.extension_config_merge(defaults={
+    #The number of events to track; if None, no limit will be applied
+    'MAX_EVENTS': 10,
+    #The maximum age of an event to track; if None, no limit will be applied
+    'MAX_AGE': 60 * 5,
+    
+    #Whether feed items should be removed when the system is reinitialised
+    'CLEAR_ON_REINIT': True,
+    
+    #Whether the list should be part of the dashboard; if False, it will appear
+    #in the methods list
+    'DISPLAY_IN_DASHBOARD': True,
+    #The positioning bias of this element in the dashboard, as an integer; if
+    #None, it will appear at the end
+    'DASHBOARD_ORDERING': None,
+    #If registering as a method, this is where its callback will be registered
+    'METHOD_PATH': '/ca/uguu/puukusoft/staticDHCPd/extension/recent-activity/render',
+    
+    #If either MODULE or NAME are None and the module is not to be rendered in
+    #the dashboard, the method-link will be hidden
+    
+    #The name of the module to which this element belongs
+    'MODULE': 'recent activity',
+    #The name of this component
+    'NAME': 'dhcp',
+}, required=[
+])
+del _config
 
 #Do not touch anything below this line
 ################################################################################
@@ -43,11 +56,9 @@ import logging
 import threading
 import time
 
-from staticdhcpdlib import config
-
 _logger = logging.getLogger('extension.recent_activity')
 
-_events = collections.deque(maxlen=MAX_EVENTS)
+_events = collections.deque(maxlen=_CONFIG['MAX_EVENTS'])
 _lock = threading.Lock()
 
 _Event = collections.namedtuple('Event', ('time', 'mac', 'ip', 'subnet', 'serial', 'method', 'pxe'))
@@ -56,7 +67,7 @@ def _drop_old_events():
     """
     Clears out any events older than `MAX_AGE`.
     """
-    max_age = time.time() - MAX_AGE
+    max_age = time.time() - _CONFIG['MAX_AGE']
     dropped = 0
     with _lock:
         while _events:
@@ -82,7 +93,7 @@ def _render(*args, **kwargs):
     with _lock:
         if not _events:
             return "No activity in the last %(max-age)i seconds" % {
-             'max-age': MAX_AGE,
+             'max-age': _CONFIG['MAX_AGE'],
             }
             
         elements = []
@@ -143,26 +154,31 @@ def _update(statistics):
 ################################################################################
 config.callbacks.statsAddHandler(_update)
 _logger.info("Prepared recent-activity-tracker for up to %(count)i events, %(max-age)i seconds old" % {
- 'count': MAX_EVENTS,
- 'max-age': MAX_AGE,
+ 'count': _CONFIG['MAX_EVENTS'],
+ 'max-age': _CONFIG['MAX_AGE'],
 })
 
-if CLEAR_ON_REINIT:
+if _CONFIG['CLEAR_ON_REINIT']:
     _logger.info("Registering callback handler to clear activity on reinitialisation...")
     config.callbacks.systemAddReinitHandler(_flush)
     
-if DISPLAY_IN_DASHBOARD:
+if _CONFIG['DISPLAY_IN_DASHBOARD']:
     _logger.info("Registering activity-tracker as a dashboard element, with ordering=%(ordering)s" % {
-     'ordering': DASHBOARD_ORDERING,
+     'ordering': _CONFIG['DASHBOARD_ORDERING'],
     })
-    config.callbacks.webAddDashboard(MODULE, NAME, _render, ordering=DASHBOARD_ORDERING)
+    config.callbacks.webAddDashboard(
+     _CONFIG['MODULE'], _CONFIG['NAME'], _render,
+     ordering=_CONFIG['DASHBOARD_ORDERING']
+    )
 else:
     _logger.info("Registering activity-tracker at '%(path)s'" % {
-     'path': METHOD_PATH,
+     'path': _CONFIG['METHOD_PATH'],
     })
     config.callbacks.webAddMethod(
-     METHOD_PATH, _render,
-     hidden=(MODULE is None or NAME is None), module=MODULE, name=NAME,
+     _CONFIG['METHOD_PATH'], _render,
+     hidden=(_CONFIG['MODULE'] is None or _CONFIG['NAME'] is None),
+     module=_CONFIG['MODULE'],
+     name=_CONFIG['NAME'],
      display_mode=config.callbacks.WEB_METHOD_TEMPLATE
     )
     
