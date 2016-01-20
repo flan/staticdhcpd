3c546120fbc26834a1587416d3d6ff7cd4b3c47b
Michael Lathrop <mlathrop@navisite.com>
2016-01-20 14:12:46 -0500

    Add Support for Multiple Responses for HTTPDB

--- staticDHCPd/extensions/official/httpdb.py
+++ staticDHCPd/extensions/official/httpdb.py
@@ -34,7 +34,7 @@ to conf.py; if anything more sophisticated is required, fork it and hack away:
         #at a time; successive requests will block; DEFAULTS TO (effectively)
         #INFINITE
         X_HTTPDB_CONCURRENCY_LIMIT = 10
-        
+
 For a list of all parameters you may define, see below.
 
 If concurrent connections to your HTTP server should be limited, use
@@ -80,7 +80,7 @@ def _parse_server_response(json_data):
         ntp_servers=json_data.get('ntp_servers'),
         extra=json_data.get('extra'),
     )
-    
+
 #Do not touch anything below this line
 ################################################################################
 import json
@@ -97,7 +97,7 @@ _logger = logging.getLogger("extension.httpdb")
 class _HTTPLogic(object):
     def __init__(self):
         from staticdhcpdlib import config
-        
+
         try:
             self._uri = config.X_HTTPDB_URI
         except AttributeError:
@@ -106,33 +106,35 @@ class _HTTPLogic(object):
         self._parameters = getattr(config, 'X_HTTPDB_PARAMETERS', {})
         self._parameter_key_mac = getattr(config, 'X_HTTPDB_PARAMETER_KEY_MAC', 'mac')
         self._post = getattr(config, 'X_HTTPDB_POST', True)
-        
+        self._default_name_servers = getattr(config, 'X_HTTPDB_DEFAULT_NAME_SERVERS', '')
+        self._default_lease_time = getattr(config, 'X_HTTPDB_DEFAULT_LEASE_TIME', 0)
+        self._default_serial = getattr(config, 'X_HTTPDB_DEFAULT_SERIAL', 0)
+
     def _lookupMAC(self, mac):
         """
         Performs the actual lookup operation; this is the first thing you should
         study when customising for your site.
         """
         global _parse_server_response
-        
         #If you need to generate per-request headers, add them here
         headers = self._headers.copy()
-        
+
         #To alter the parameters supplied with the request, alter this
         parameters = self._parameters.copy()
-        #Dynamic items 
+        #Dynamic items
         parameters.update({
             self._parameter_key_mac: str(mac),
         })
-        
+
         #You can usually ignore this if-block, though you could strip out whichever method you don't use
         if self._post:
             data = json.dumps(parameters)
-            
+
             headers.update({
                 'Content-Length': str(len(data)),
                 'Content-Type': 'application/json',
             })
-            
+
             request = urllib2.Request(
                 self._uri, data=data,
                 headers=headers,
@@ -145,33 +147,38 @@ class _HTTPLogic(object):
              },
              headers=headers,
             )
-            
-        _logger.debug("Sending request to '%(uri)s' for '%(mac)s'..." % {
+
+        _logger.debug("Sending request to '%(uri)s' for '%(params)s'..." % {
             'uri': self._uri,
-            'mac': str(mac),
+            'params': parameters,
         })
+
         try:
             response = urllib2.urlopen(request)
             _logger.debug("MAC response received from '%(uri)s' for '%(mac)s'" % {
                 'uri': self._uri,
                 'mac': str(mac),
             })
-            result = json.loads(response.read())
-            
-            if not result: #The server sent back 'null' or an empty object
+            results = json.loads(response.read())
+            if not isinstance(results, (list,tuple)):
+                results = [results]
+
+            if not results: #The server sent back 'null' or an empty object
                 _logger.debug("Unknown MAC response from '%(uri)s' for '%(mac)s'" % {
                     'uri': self._uri,
                     'mac': str(mac),
                 })
                 return None
-                
-            definition = _parse_server_response(result)
-            
+
+            _logger.debug("Results from HTTPDB call: %s" % results)
+
             _logger.debug("Known MAC response from '%(uri)s' for '%(mac)s'" % {
                 'uri': self._uri,
                 'mac': str(mac),
             })
-            return definition
+
+            return [_parse_server_response(self._set_defaults(result)) for result in results]
+
         except Exception, e:
             _logger.error("Failed to lookup '%(mac)s' on '%(uri)s': %(error)s" % {
                 'uri': self._uri,
@@ -179,19 +186,35 @@ class _HTTPLogic(object):
                 'error': str(e),
             })
             raise
-            
+
+    def _set_defaults(self, json_data):
+        """
+        Set the default values on a server response if they do not
+         already have usable values
+
+        :param dictionary json_data: Dictionary containing response data
+        :return dictionary: The modified dictionary with defaults
+        """
+        if not json_data.get('serial'):
+            json_data['serial'] = self._default_serial
+        if not json_data.get('domain_name_servers'):
+            json_data['domain_name_servers'] = self._default_name_servers
+        if not json_data.get('lease_time'):
+            json_data['lease_time'] = self._default_lease_time
+        return json_data
+
 class HTTPDatabase(Database, _HTTPLogic):
     def __init__(self):
         _HTTPLogic.__init__(self)
-        
+
     def lookupMAC(self, mac):
         return self._lookupMAC(mac)
-        
+
 class HTTPCachingDatabase(CachingDatabase, _HTTPLogic):
     def __init__(self):
+        from staticdhcpdlib import config
         if hasattr(config, 'X_HTTPDB_CONCURRENCY_LIMIT'):
             CachingDatabase.__init__(self, concurrency_limit=config.X_HTTPDB_CONCURRENCY_LIMIT)
         else:
             CachingDatabase.__init__(self)
         _HTTPLogic.__init__(self)
-        
