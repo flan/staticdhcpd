a74a5f48eabfa382ef4a1fd6edef044c811be52a
Neil Tallim <flan@uguu.ca>
2019-10-24 21:29:19 -0600

    Update mysql.sql
    Looks like VARCHAR without a qualified length isn't valid in MariaDB. TEXT seems to have an ambiguous default, but declaring it as DEFAULT NULL then causes the engines to use the TINYTEXT type under the hood, which is pretty close to ideal.

--- staticDHCPd/databases/mysql.sql
+++ staticDHCPd/databases/mysql.sql
@@ -5,7 +5,7 @@ CREATE TABLE subnets (
     subnet CHAR(18) NOT NULL, -- A human-readable subnet-identifier, large enough to hold a CIDR mask.
     serial SMALLINT UNSIGNED NOT NULL DEFAULT 0, -- A means of allowing a subnet to be reused, just in case you have two 192.168.1.0/24s.
     lease_time MEDIUMINT UNSIGNED NOT NULL, -- The number of seconds a "lease" is good for. This can be massive unless properties change often.
-    gateway VARCHAR, -- A comma-separated list of IPv4 gateways to supply to clients; may be null.
+    gateway TEXT DEFAULT NULL, -- A comma-separated list of IPv4 gateways to supply to clients; may be null.
     subnet_mask CHAR(15), -- The IPv4 subnet mask to supply to clients; may be null.
     broadcast_address CHAR(15), -- The IPv4 broadcast address to supply to clients; may be null.
     ntp_servers CHAR(50), -- A comma-separated list of IPv4 addresses pointing to NTP servers; limit 3; may be null.
